c6da2faac096a0baba8a643c0eb173d8b8660d8c
Nitu Dubey <nitutiwari01@gmail.com>
2023-08-25 10:37:29 -0400

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,27 @@
 
 import React from "react";
 import './../styles/App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./Navbar";
+import Item1 from "./pages/Item1";
+import Item2 from "./pages/Item2";
+import Item3 from "./pages/Item3";
+
 
 const App = () => {
   return (
-    <div>
+    <BrowserRouter>
         {/* Do not remove the main div */}
-    </div>
+        
+        
+        <Routes>
+          <Route path="/" element={<Navbar/>}/>
+          <Route path="/items/1" element={<Item1/>}/>
+          <Route path="/items/2" element={<Item2/>}/>
+          <Route path="/items/3" element={<Item3/>}/>
+        </Routes>
+        
+    </BrowserRouter>
   )
 }
 
